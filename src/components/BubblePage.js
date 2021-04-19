41155219a890f15bada8ff03ca6f1befccab0c3d
unknown <Alikhan7273@gmail.com>
2021-04-19 00:03:41 -0500

    working

--- src/components/BubblePage.js
+++ src/components/BubblePage.js
@@ -1,13 +1,28 @@
 import React, { useEffect, useState } from "react";
 
+import { axiosWithAuth } from "../helpers/axiosWithAuth";
+
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
 
 const BubblePage = () => {
   const [colorList, setColorList] = useState([]);
 
+  useEffect(() => {
+    axiosWithAuth()
+      .get("/api/colors")
+      .then((res) => {
+        setColorList(res.data);
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  }, []);
+
   return (
-    <div className="container">
+    <div>
+      {console.log("Hello world from BubblePage.js")}
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
     </div>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,65 @@
-import React, { useEffect } from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useHistory } from "react-router-dom";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
 
-  useEffect(()=>{
-    // make a post request to retrieve a token from the api
-    // when you have handled the token, navigate to the BubblePage route
+  const [credentials, setCredentials] = useState({
+    username: "",
+    password: "",
   });
-  
-  const error = "";
-  //replace with error state
+
+  const { push } = useHistory();
+
+  const onChange = (e) => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // useEffect(() => {
+  //   // make a post request to retrieve a token from the api
+  //   // when you have handled the token, navigate to the BubblePage route
+  // });
+
+  const login = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/api/login", credentials)
+      .then((res) => {
+        console.log("res", res);
+        localStorage.setItem("token", res.data.payload);
+        push("/colors");
+      })
+      .catch((err) => {
+        console.log(err.response);
+      });
+  };
 
   return (
-    <div>
-      <h1>Welcome to the Bubble App!</h1>
-      <div data-testid="loginForm" className="login-form">
-        <h2>Build login form here</h2>
-      </div>
-
-      <p data-testid="errorMessage" className="error">{error}</p>
+    <div className="login-form">
+      <form onSubmit={login}>
+        <label htmlFor="username">UserName:</label>
+        <input
+          type="text"
+          name="username"
+          value={credentials.username}
+          onChange={onChange}
+        />
+        <br />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          value={credentials.password}
+          onChange={onChange}
+        />
+        <button>Log in</button>
+      </form>
     </div>
   );
 };
